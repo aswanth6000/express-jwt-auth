8298761fa3a89afe7ac747e991151b807bb09a58
aswanth6000 <aswanth682@gmail.com>
2023-12-26 11:05:34 +0530

    revert commit

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,22 @@ import dotenv from 'dotenv'
 dotenv.config()
 const jwtSecret: Secret = process.env.JWT_KEY || 'defaultSecret'
 
+interface ExtendedRequest extends Request {
+    user?: string | JwtPayload | undefined;
+  }
 
-
-export default function verifyToken(req: any, res: Response, next: NextFunction) {
+export default function verifyToken(req: ExtendedRequest, res: Response, next: NextFunction) {
   let authHeader = req.headers.authorization;
   if (authHeader === undefined) {
     res.status(401).send({ error: 'No token provided' });
   }
   let token = authHeader?.split(' ')[1];
   if (token) {
-    jwt.verify(token, jwtSecret, (err: any, decoded: any) => {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
       if (err) {
         res.status(500).send({ message: 'Authentication failed' });
       } else {
-        (req as any).user = decoded;
+        (req as ExtendedRequest).user = decoded;
         next();
       }
     });
